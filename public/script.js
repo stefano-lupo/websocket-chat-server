f000b7d3ea194cdf723c6179c3f3fbf21874ee4a
stefano <lupos@tcd.ie>
2017-09-26 13:07:00 +0100

    Styled and updated code

--- public/script.js
+++ public/script.js
@@ -1,18 +1,74 @@
+$(document).ready(function () {
+    var ws;
+    var name, avatar;
 
-$(document).ready(function() {
+    $("#join").submit(function( event ) {
+        event.preventDefault();
 
- var ws = new WebSocket('ws://86.43.98.198:3000', 'echo-protocol');
+        // Save credentials
+        name = $("#name").val();
+        avatar = $("#avatar").val();
+      
+        // Connnect to Server
+        ws =  new WebSocket('ws://localhost:3000', 'echo-protocol');
 
- $("#send").click(function() { 
-    var message = $("#message").val();
-    ws.send(message);
-   
- });
+        // Send join messgae  
+        ws.onopen = function() {
+            var message = {
+                name: name,
+                avatar: avatar,
+                message: "joined the chatroom.",
+                date: new Date()
+            }
+            ws.send(JSON.stringify(message));
+        };
 
- ws.addEventListener("message", function(e) {
-    // The data is simply the message that we're sending back
-    var msg = e.data;
-    
-    $("#chatlog").append("</br>" + msg);
- });
+        // Allow messages to be sent
+        $("#message").prop('disabled', false);
+        $("#send-button").removeClass("disabled btn-danger").addClass("btn-success");
+
+        // Add Listener for receiving messages
+        ws.addEventListener("message", function (e) {
+            var msg = JSON.parse(e.data);
+            if(Array.isArray(msg)) {
+                msg.forEach(function (message) {
+                    addMessage(message);   
+                });
+            } else {
+                addMessage(msg);
+            }
+
+            // Scroll to bottom of chat div
+            $('#chat-div').scrollTop($('#chat-div')[0].scrollHeight);
+        });
+    });
+
+    // Add listener for sending messages
+    $("#send").submit(function( event ) {
+        event.preventDefault();
+
+        // Package up the message object
+        var message = {
+            name: name,
+            avatar: avatar,
+            message: $("#message").val(),
+            date: new Date()
+        }
+
+        // Send the message object (serialized as a string)
+        ws.send(JSON.stringify(message));
+    });  
+
+    // Some markup for the messages
+    function addMessage(message) {
+        $("#chatlog").append('<li id="message-li">'
+            + '<div><img id="avatar-pic" src="'
+            + "  " + message.avatar + '">' 
+            + "<h4>" + message.name + "</h4>"
+            + '<p id="date-string">' + new Date(message.date).toLocaleTimeString() + "</p></div>" 
+            + message.message 
+            + "<hr>"   
+            + "</li>"
+        );
+    }
 });
--- server/index.js
+++ server/index.js
@@ -1,46 +1,44 @@
 var http = require('http');
-var server = http.createServer(function(request, response) {});
+var server = http.createServer(function (request, response) { });
 
-server.listen(3000, function() {
-    console.log((new Date()) + ' Server is listening on port 3000');
+server.listen(3000, function () {
+    console.log((new Date()) + ': Server is listening on port 3000');
 });
 
-var WebSocketServer = require('websocket').server; wsServer = 
-new WebSocketServer({
-    httpServer: server
-});
+var WebSocketServer = require('websocket').server;
+wsServer = new WebSocketServer({ httpServer: server });
 
-var count = 0;
-var clients = [];
+var id = 0;
+var clients = new Map();
+var messages = [];
 
-wsServer.on('request', function(r){
-    // Code here to run on connection
-	var connection = r.accept('echo-protocol', r.origin);
+function sendToAllClients(message) {
+    clients.forEach(function(client) {
+        client.sendUTF(message);
+    })
+}
 
-// Specific id for this client & increment count 
-var id = count++; // Store the connection method so we can loop through & 
-// contact all clients
-clients[id] = connection;
+wsServer.on('request', function (r) {
+    // Envoked when client connects, send them all previous messages
+    var connection = r.accept('echo-protocol', r.origin);
+    connection.id = id++;
+    clients.set(id, connection);
+    console.log((new Date()) + ' Connection accepted [' + connection.id + ']');
 
-console.log((new Date()) + ' Connection accepted [' + id + ']');
+    messages.forEach(function(message) {
+        sendToAllClients(message);
+    });
 
+    // Envoked when a message is sent
+    connection.on('message', function (message) {
+        var msgString = message.utf8Data;
+        messages.push(msgString);
+        sendToAllClients(msgString);
+    });
 
-// Create event listener connection.on('message', 
-connection.on('message', function(message) {
-    // The string message that was sent to us
-    var msgString = message.utf8Data;
-    // Loop through all clients
-    for(var i in clients){
-        // Send a message to the client with the message
-        clients[i].sendUTF(msgString);
-    }
-});
-
-
-connection.on('close', function(reasonCode, description) {
-    delete clients[id];
-    console.log((new Date()) + ' Peer ' + 
-connection.remoteAddress + ' disconnected.');
-});
-
+    // Envoked when client disconnects
+    connection.on('close', function (reasonCode, description) {
+        clients.delete(connection.id);
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
 });
